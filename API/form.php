6860dc98f57f9dd050d80ea136e72e7844ab538b
alpayOnal <alpaycom@gmail.com>
2013-02-20 11:56:01 +0200

    Api kısmı hazırlandı

---
+++ API/form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<body>
+
+analysis/hourly/2013-02-05 15:00
+<form method="post" action="analysis/hourly/2013-02-05%2015%3A00">
+<input type="submit" name="submit" value="send" >
+</form>
+
+demo/analysis/cached
+<form method="post" action="analysis/cached">
+<input type="submit" name="submit" value="send" >
+</form>
+
+demo/analysis/51223196d09acb4919000000
+<form method="post" action="analysis/51223196d09acb4919000000">
+<input type="submit" name="submit" value="send" >
+</form>
+
+demo/analysis/51223196d09acb4919000000/events/pepsi%20i%C3%A7
+<form method="post" action="analysis/51223196d09acb4919000000/events/pepsi%20i%C3%A7">
+<input type="submit" name="submit" value="send" >
+</form>
+
+</body>
+</html>
---
+++ API/request.php
@@ -0,0 +1,46 @@
+<?php
+header("Content-Type:text/plain;charset=utf-8");
+require_once("../trendDetection.php");
+class trendApi{
+
+	public function run(){
+
+		$td=new TrendDetection();
+		$r=$_REQUEST;
+		switch ($r['task']){
+
+			case 'startAnalysis':
+
+				if (isset($r['interval'],$r['date']))
+					print_r($td->setAnalysisInterval($r['interval'],$r['date']));				
+
+			break;
+
+			case 'cached':
+					print_r($td->getCachedAnalysis());
+			break;
+
+			case 'getAnalysis':
+
+				if (isset($r['analysisId']))
+					print_r($td->getCachedAnalysis($r['analysisId']));
+
+			break;
+
+			case 'getEventDetail':
+				if (isset($r['analysisId'],$r['eventText'])){					
+					print_r($td->getEventDetail($r['analysisId'],$r['eventText']));
+				}
+			break;
+
+			default: 
+				echo "parameters are required";die();
+			break;
+		}
+	}
+}
+
+$api=new trendApi();
+$api->run();
+
+?>
