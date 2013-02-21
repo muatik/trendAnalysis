83d633ff26e426e70861249e9d6572214720d590
alpayOnal <alpaycom@gmail.com>
2013-02-21 04:57:46 +0200

    .htaccess and request.php path changed

--- request.php
+++ request.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type:text/plain;charset=utf-8");
-require_once("../trendDetection.php");
+require_once("trendDetection.php");
 class trendApi{
 
 	public function run(){
@@ -11,24 +11,29 @@ class trendApi{
 
 			case 'startAnalysis':
 
-				if (isset($r['interval'],$r['date']))
-					json_encode($td->setAnalysisInterval($r['interval'],$r['date']));				
+				if (isset($r['interval'],$r['date'])){					
+					$td->setAnalysisInterval($r['interval'],$r['date']);
+					echo json_encode($td->detect());
+				}
 
 			break;
 
-			case 'cached':
-					json_encode($td->getCachedAnalysis());
+			case 'cached':				
+					json_encode($td->getListOfCachedAnalyses());
 			break;
 
 			case 'getAnalysis':
 
-				if (isset($r['analysisId']))
+				if (isset($r['analysisId'])){
+					echo $r['analysisId'];
 					json_encode($td->getCachedAnalysis($r['analysisId']));
+				}
 
 			break;
 
 			case 'getEventDetail':
-				if (isset($r['analysisId'],$r['eventText'])){					
+				if (isset($r['analysisId'],$r['eventText'])){
+					echo $r['analysisId'].'---'.$r['eventText'];
 					json_encode($td->getEventOfAnalysis($r['analysisId'],$r['eventText']));
 				}
 			break;
